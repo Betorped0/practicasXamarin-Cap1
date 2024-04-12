f2a50c5a5261523b5a58f4c493884691ec9f43a2
Victor4You <patovm_1998@hotmail.com>
2024-04-12 14:53:10 -0600

    rama nueva actual

---
+++ DoToo/TodoItemRepository.cs
@@ -0,0 +1,77 @@
+﻿using DoToo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using SQLite;
+using System.IO
+namespace DoToo.Repositories
+{
+    private SQLiteAsyncConnection connection;
+
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        public event EventHandler<TodoItem> OnItemAdded;
+        public event EventHandler<TodoItem> OnItemUpdated;
+        public async Task<List<TodoItem>> GetItems()
+        {
+            return null; // Just to make it build
+        }
+        public async Task AddItem(TodoItem item)
+        {
+        }
+        public async Task UpdateItem(TodoItem item)
+        {
+        }
+        public async Task AddOrUpdate(TodoItem item)
+        {
+            if (item.Id == 0)
+            {
+                await AddItem(item);
+            }
+            else
+            {
+                await UpdateItem(item);
+            }
+            private async Task CreateConnection()
+            {
+                if (connection != null)
+                {
+                    return;
+                }
+                var documentPath = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments);
+                var databasePath = Path.Combine(documentPath, "TodoItems.db");
+                connection = new SQLiteAsyncConnection(databasePath);
+                await connection.CreateTableAsync<TodoItem>();
+                if (await connection.Table<TodoItem>().CountAsync() == 0)
+                {
+                    await connection.InsertAsync(new TodoItem()
+                    {
+                        Title = "Welcome to DoToo",
+                        Due = DateTime.Now
+                    });
+                }
+            }
+            GetItems()
+                public async Task<List<TodoItem>> GetItems()
+            {
+                await CreateConnection();
+                return await connection.Table<TodoItem>().ToListAsync();
+            }
+            AddItem()
+                public async Task AddItem(TodoItem item)
+            {
+                await CreateConnection();
+                await connection.InsertAsync(item);
+                OnItemAdded?.Invoke(this, item);
+            }
+            UpdateItem()
+                public async Task UpdateItem(TodoItem item)
+            {
+                await CreateConnection();
+                await connection.UpdateAsync(item);
+                OnItemUpdated?.Invoke(this, item);
+            }
+
+        }
