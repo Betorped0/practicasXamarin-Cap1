5d1c7f077e32721e992cb6b70f5f1bbf44fec02b
Victor4You <patovm_1998@hotmail.com>
2024-04-16 14:11:42 -0600

    Esto es el capitulo 2

---
+++ News/News/News/MainShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using News.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace News
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mainshell : Shell
+    {
+        public Mainshell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("articleview", 
+                typeof(ArticleView));
+        }
+    }
+}
